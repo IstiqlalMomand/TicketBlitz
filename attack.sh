133c82f91bf9c274bbc9d5f9500278972a8ebcbb
Faisal Khan <faisal@MacBook-Pro-von-Faisal.local>
2026-01-26 22:12:44 +0100

    just a test commit

--- attack.sh
+++ attack.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "🚀 Launching MASSIVE Attack (500 Requests)..."
 # Send 500 requests in the background
-for i in {1..500}
-do
-   curl -X POST "http://localhost:8080/api/bookings?eventId=2&userId=$i" > /dev/null 2>&1 &
+for i in {1..5000}; do
+  curl -X POST "http://localhost:8080/api/bookings?eventId=2&userId=$i" >/dev/null 2>&1 &
 done
 echo "✅ Requests sent. Waiting for the dust to settle..."
 wait
--- src/main/java/com/ticketblitz/ticketblitz/TicketBlitzApplication.java
+++ src/main/java/com/ticketblitz/ticketblitz/TicketBlitzApplication.java
@@ -3,7 +3,6 @@ package com.ticketblitz.ticketblitz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class TicketBlitzApplication {
 
     public static void main(String[] args) {
